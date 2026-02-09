7afcbf59f0026ae9a3aa12436bb18cba0c5ade66
Khairul Bashar Naeeim <mdkhairulb01@gmail.com>
2026-02-09 12:37:52 +0600

    schema has been seperated and connected well

--- src/app/module/specialty/specialty.controller.ts
+++ src/app/module/specialty/specialty.controller.ts
@@ -2,65 +2,52 @@
 import { Request, Response } from "express";
 import { specialtyServices } from "./specialty.service";
 import { catchAsync } from "../../shared/catchAsync";
+import { sendResponse } from "../../shared/sendResponse";
 
 const createSpecialty = catchAsync(async (req: Request, res: Response) => {
     console.log("Inside Real Controller!!");
     const payload = req.body;
     const specialty = await specialtyServices.createSpecialty(payload);
-    res.status(201).json({
+    sendResponse(res, {
+        httpStatusCode: 201,
         success: true,
         message: "Specialty created successfully",
         data: specialty
-    });
+    })
 })
 
 const getAllSpecialties = catchAsync(async (req: Request, res: Response) => {
     const specialties = await specialtyServices.getAllSpecialties();
-    res.status(200).json({
+    sendResponse(res, {
+        httpStatusCode: 200,
         success: true,
         message: "Specialties retrieved successfully",
         data: specialties
     })
 })
 
-const deleteSpecialty = async (req: Request, res: Response) => {
-    try {
-        const { id } = req.params;
-        const deletedSpecialty = await specialtyServices.deleteSpecialty(id as string);
-        res.status(200).json({
-            success: true,
-            message: "Specialty deleted successfully",
-            data: deletedSpecialty
-        });
-    } catch (error: any) {
-        console.log(error);
-        res.status(500).json({
-            success: false,
-            message: "An unexpected error occurred.",
-            error: error.message
-        });
-    }
-}
+const deleteSpecialty = catchAsync(async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const deletedSpecialty = await specialtyServices.deleteSpecialty(id as string);
+    sendResponse(res, {
+        httpStatusCode: 200,
+        success: true,
+        message: "Specialty deleted successfully",
+        data: deletedSpecialty
+    })
+})
 
-const updateSpecialty = async (req: Request, res: Response) => {
-    try {
-        const { id } = req.params;
-        const payload = req.body;
-        const updatedSpecialty = await specialtyServices.updateSpecialty(id as string, payload);
-        res.status(200).json({
-            success: true,
-            message: "Specialty updated successfully",
-            data: updatedSpecialty
-        });
-    } catch (error: any) {
-        console.log(error);
-        res.status(500).json({
-            success: false,
-            message: "An unexpected error occurred.",
-            error: error.message
-        });
-    }
-}
+const updateSpecialty = catchAsync(async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const payload = req.body;
+    const updatedSpecialty = await specialtyServices.updateSpecialty(id as string, payload);
+    sendResponse(res, {
+        httpStatusCode: 200,
+        success: true,
+        message: "Specialty updated successfully",
+        data: updatedSpecialty
+    })
+})
 
 
 export const specialtyController = {
