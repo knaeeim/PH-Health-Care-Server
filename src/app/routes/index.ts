a3f3ccd63abaf1f562426a5ff608ee89522d87a9
Khairul Bashar Naeeim <mdkhairulb01@gmail.com>
2026-02-10 00:24:10 +0600

    Better Auth User additional Fields and Other things Done

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { specialtyRouter } from "../module/specialty/specialty.routes";
+import { authRouter } from "../module/auth/auth.routes";
 
-const router = Router(); 
+const router = Router();
 
 router.use("/specialties", specialtyRouter);
+router.use("/auth", authRouter)
 
 
 export const indexRouter = router;
