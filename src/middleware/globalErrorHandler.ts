bc6b2ece2872e055b2d1696832320c0e0c0fe67b
Khairul Bashar Naeeim <mdkhairulb01@gmail.com>
2026-02-15 01:38:32 +0600

    Error Handle for inGeneral

--- src/middleware/globalErrorHandler.ts
+++ src/middleware/globalErrorHandler.ts
@@ -15,24 +15,28 @@ export const globalErrorHandler = (err: any, req: Request, res: Response, next: 
 
     let statusCode: number = status.INTERNAL_SERVER_ERROR;
     let message = err.message || "Internal Server Error";
+    let stack: string | undefined = undefined;
 
-    const errorSources : TErrorSources[] = [];
+    const errorSources: TErrorSources[] = [];
 
-    if(err instanceof z.ZodError) {
+    if (err instanceof z.ZodError) {
         const simplifiedError = handleZodError(err);
         statusCode = simplifiedError.statusCode as number;
         message = simplifiedError.message;
         errorSources.push(...simplifiedError.errorSources!);
+    } else if (err instanceof Error) {
+        statusCode = status.INTERNAL_SERVER_ERROR;
+        message = err.message;
+        stack = err.stack;
     }
 
-    const errorResponse : TErrorResponse = {
-        success : false,
+    const errorResponse: TErrorResponse = {
+        success: false,
         message,
         errorSources,
+        stack: envVars.NODE_ENV === "development" ? stack : undefined,
         error: envVars.NODE_ENV === "development" ? err.message : undefined,
     }
 
-
     res.status(statusCode).json(errorResponse)
-
 }
