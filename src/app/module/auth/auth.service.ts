3813a5261c50f5184a70203e45a3971a117a6672
Khairul Bashar Naeeim <mdkhairulb01@gmail.com>
2026-02-20 16:31:17 +0600

    if needPasswordChange is true then we will show a popup in front change password without chaning you can't go further. in Backend we are checking needPasswordChange is true? then we are just making it false in Database that part of code added in Change password Service layer

--- src/app/module/auth/auth.service.ts
+++ src/app/module/auth/auth.service.ts
@@ -231,6 +231,17 @@ const changePassword = async (payload: IChangePasswordPayload, sessionToken: str
             })
         })
 
+        if (session.user.needPasswordChange) {
+            await prisma.user.update({
+                where: {
+                    id: session.user.id
+                },
+                data: {
+                    needPasswordChange: false
+                }
+            })
+        }
+
         const accessToken = tokenUtils.getAccessToken({
             userId: session.user.id,
             email: session.user.email,
