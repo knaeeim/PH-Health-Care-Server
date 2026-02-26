e6094339e08c3fe5b7255cdce4ad31e6d1332964
Khairul Bashar Naeeim <mdkhairulb01@gmail.com>
2026-02-27 00:39:23 +0600

    Cloudinary deleted function implemented

--- src/middleware/globalErrorHandler.ts
+++ src/middleware/globalErrorHandler.ts
@@ -20,6 +20,11 @@ export const globalErrorHandler = async (err: any, req: Request, res: Response, 
         await deleteFromCloudinary(req.file.path);
     }
 
+    if (req.files && Array.isArray(req.files) && req.files.length > 0) {
+        const imageUrls = req.files.map((file) => file.path);
+        await Promise.all(imageUrls.map((url) => deleteFromCloudinary(url)));
+    }
+
     let statusCode: number = status.INTERNAL_SERVER_ERROR;
     let message = err.message || "Internal Server Error";
     let stack: string | undefined = undefined;
