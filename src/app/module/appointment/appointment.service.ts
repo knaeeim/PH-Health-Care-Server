bf0ee246a4dba3b9af8da81b006b0801537463eb
Khairul Bashar Naeeim <mdkhairulb01@gmail.com>
2026-03-10 02:01:41 +0600

    BookAppointmentwithPayLater has done

--- src/app/module/appointment/appointment.service.ts
+++ src/app/module/appointment/appointment.service.ts
@@ -261,10 +261,71 @@ const getAllAppointments = async () => {
     return appointments;
 }
 
+const bookAppointmentWithPayLater = async (payload: IBookAppointmentPayload, user: IRequestUser) => {
+    const patientData = await prisma.patient.findUniqueOrThrow({
+        where: {
+            email: user.email
+        }
+    });
+
+    const doctorData = await prisma.doctor.findUniqueOrThrow({
+        where: {
+            id: payload.doctorId,
+            isDeleted: false
+        }
+    });
+
+    const scheduleData = await prisma.schedule.findUniqueOrThrow({
+        where: {
+            id: payload.scheduleId
+        }
+    })
+
+    const doctorScheduleData = await prisma.doctorSchedule.findUniqueOrThrow({
+        where: {
+            doctorId_scheduleId: {
+                doctorId: doctorData.id,
+                scheduleId: scheduleData.id
+            }
+        }
+    })
+
+    const videoCalligId = String(uuidv7());
+
+    const result = await prisma.$transaction(async (tx) => {
+        const appointmentData = await tx.appointment.create({
+            data: {
+                doctorId: payload.doctorId,
+                patientId: patientData.id,
+                scheduleId: doctorScheduleData.scheduleId,
+                videoCallingId: videoCalligId
+            }
+        })
+
+        await tx.doctorSchedule.update({
+            where: {
+                doctorId_scheduleId: {
+                    doctorId: payload.doctorId,
+                    scheduleId: payload.scheduleId
+                }
+            },
+            data: {
+                isBooked: true
+            }
+        })
+        return appointmentData;
+    })
+
+    return {
+        appointment: result
+    }
+}
+
 export const appointmentService = {
     bookAppointment,
     getMyAppointments,
     changeAppointmentStatus,
     getAllAppointments,
     getMySingleAppointment,
+    bookAppointmentWithPayLater
 }
